9335f8584af761e4c2c6fc76b880c7a4f7ecd9d7
Vishnu Nair <vn75670@gmail.com>
2026-01-19 19:47:00 +0530

    remove all occurance java program

---
+++ tut38.java
@@ -0,0 +1,25 @@
+//Remmove all occurances of the same charchater
+//aabca
+public class tut38 {
+
+    public static String alloccurance(String s) {
+        // Step 1 base case
+        if (s.length() == 0)
+            return "";
+
+        // step 2 get first char
+        char ch = s.charAt(0);
+
+        String rest = s.substring(1);
+
+        if (ch == 'a')
+            return alloccurance(rest);
+        else
+            return ch + alloccurance(rest);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(alloccurance("aabca"));
+    }
+}
